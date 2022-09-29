ab191cbea28cb8711e269b15b9ee418d4338de21
Jean Chiclla <j-c_f@outlook.com>
2022-09-28 23:44:57 -0500

    Se obtiene data de firebase

--- public/index.js
+++ public/index.js
@@ -15,11 +15,22 @@
  */
 'use strict';
 
-(function() {
+var DataDB;
+async function loadData() {
+  const res = await fetch(
+    "https://fbase-lienzo-anden-default-rtdb.firebaseio.com/data.json"
+  );
+  DataDB = await res.json();
+  DataDB.linkHotspots = [];
+
+  init();
+}
+
+function init() {
   var Marzipano = window.Marzipano;
   var bowser = window.bowser;
   var screenfull = window.screenfull;
-  var data = window.APP_DATA;
+  var data = window.DataDB;
 
   // Grab elements from DOM.
   var panoElement = document.querySelector('#pano');
@@ -89,10 +100,10 @@
     });
 
     // Create link hotspots.
-    data.linkHotspots.forEach(function(hotspot) {
-      var element = createLinkHotspotElement(hotspot);
-      scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-    });
+    // data.linkHotspots.forEach(function(hotspot) {
+    //   var element = createLinkHotspotElement(hotspot);
+    //   scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
+    // });
 
     // Create info hotspots.
     data.infoHotspots.forEach(function(hotspot) {
@@ -118,7 +129,7 @@
   }
 
   // Set handler for autorotate toggle.
-  autorotateToggleElement.addEventListener('click', toggleAutorotate);
+  // autorotateToggleElement.addEventListener('click', toggleAutorotate);
 
   // Set up fullscreen mode, if supported.
   if (screenfull.enabled && data.settings.fullscreenButton) {
@@ -662,7 +673,7 @@
     var toggle = function () {
       // let isActive = modal.classList.contains("visible");
       modal.classList.toggle("visible");
-      hotspot.isVisible = modal.classList.toggle("visible");
+      // hotspot.isVisible = modal.classList.toggle("visible");
 
     };
 
@@ -693,85 +704,12 @@
       .querySelector(".header-wrapper_icon")
       .addEventListener("click", () => {
         modal.classList.toggle("visible");
-        hotspot.isVisible = false;
+        // hotspot.isVisible = false;
       });
 
     button_link.addEventListener("click", () =>
       openLink(hotspot.lote, hotspot.manzana)
     );
-
-    // Prevent touch and scroll events from reaching the parent element.
-    // This prevents the view control logic from interfering with the hotspot.
-    stopTouchAndScrollEventPropagation(wrapper);
-
-    return wrapper;
-  }
-
-  function createInfoHotspotElement(hotspot) {
-
-    // Create wrapper element to hold icon and tooltip.
-    var wrapper = document.createElement('div');
-    wrapper.classList.add('hotspot');
-    wrapper.classList.add('info-hotspot');
-
-    // Create hotspot/tooltip header.
-    var header = document.createElement('div');
-    header.classList.add('info-hotspot-header');
-
-    // Create image element.
-    var iconWrapper = document.createElement('div');
-    iconWrapper.classList.add('info-hotspot-icon-wrapper');
-    var icon = document.createElement('img');
-    icon.src = 'img/info.png';
-    icon.classList.add('info-hotspot-icon');
-    iconWrapper.appendChild(icon);
-
-    // Create title element.
-    var titleWrapper = document.createElement('div');
-    titleWrapper.classList.add('info-hotspot-title-wrapper');
-    var title = document.createElement('div');
-    title.classList.add('info-hotspot-title');
-    title.innerHTML = hotspot.title;
-    titleWrapper.appendChild(title);
-
-    // Create close element.
-    var closeWrapper = document.createElement('div');
-    closeWrapper.classList.add('info-hotspot-close-wrapper');
-    var closeIcon = document.createElement('img');
-    closeIcon.src = 'img/close.png';
-    closeIcon.classList.add('info-hotspot-close-icon');
-    closeWrapper.appendChild(closeIcon);
-
-    // Construct header element.
-    header.appendChild(iconWrapper);
-    header.appendChild(titleWrapper);
-    header.appendChild(closeWrapper);
-
-    // Create text element.
-    var text = document.createElement('div');
-    text.classList.add('info-hotspot-text');
-    text.innerHTML = hotspot.text;
-
-    // Place header and text into wrapper element.
-    wrapper.appendChild(header);
-    wrapper.appendChild(text);
-
-    // Create a modal for the hotspot content to appear on mobile mode.
-    var modal = document.createElement('div');
-    modal.innerHTML = wrapper.innerHTML;
-    modal.classList.add('info-hotspot-modal');
-    document.body.appendChild(modal);
-
-    var toggle = function() {
-      wrapper.classList.toggle('visible');
-      modal.classList.toggle('visible');
-    };
-
-    // Show content when hotspot is clicked.
-    wrapper.querySelector('.info-hotspot-header').addEventListener('click', toggle);
-
-    // Hide content when close icon is clicked.
-    modal.querySelector('.info-hotspot-close-wrapper').addEventListener('click', toggle);
 
     // Prevent touch and scroll events from reaching the parent element.
     // This prevents the view control logic from interfering with the hotspot.
@@ -809,13 +747,15 @@
     return null;
   }
 
-  // Display the initial scene.
-  switchScene(scenes[0]);
-
+  
   document.addEventListener("dragstart", function (evt) {
     if (evt.target.tagName == "IMG") {
       evt.preventDefault();
     }
   });
+  
+  // Display the initial scene.
+  switchScene(scenes[0]);
+}
 
-})();
+loadData();
